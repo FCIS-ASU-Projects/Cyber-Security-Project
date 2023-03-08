d21ecc3b922496cd8fc45c5a401cf2607c4ab58a
Sara Mahmoud <saramahmoud5005@gmail.com>
2023-03-08 20:28:04 +0200

    Update RailFence.cs

--- SecurityPackage[Template]/securitylibrary/MainAlgorithms/RailFence.cs
+++ SecurityPackage[Template]/securitylibrary/MainAlgorithms/RailFence.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +11,50 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            plainText=plainText.ToLower();
+            cipherText=cipherText.ToLower();
+
+            int keyDepth = 1;
+
+            for(int i=keyDepth; i<plainText.Length; ++i)
+                if (Encrypt(plainText, i).ToLower() == cipherText) keyDepth = i;
+
+            return keyDepth;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            int numOfColmns = (int)Math.Ceiling((double)cipherText.Length / (double)key);
+            String plaintext = "";
+
+            for (int i = 0; i < numOfColmns; ++i)
+            {
+                for (int j = i; j < cipherText.Length; j+=numOfColmns)
+                    plaintext += cipherText[j];
+            }
+
+            return plaintext;
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            String cipherText="";
+            List<List<char>> cipherList=new List<List<char>>();
+
+            for(int i=0; i<key; ++i)
+            {
+                cipherList.Add(new List<char>());
+                for(int j=i; j<plainText.Length; j+=key)
+                    cipherList[i].Add(plainText[j]);
+            }
+
+            for(int i=0; i<cipherList.Count; ++i)
+            {
+                for(int j=0; j < cipherList[i].Count; ++j)
+                    cipherText += cipherList[i][j];
+            }
+
+            return cipherText;
         }
     }
 }
