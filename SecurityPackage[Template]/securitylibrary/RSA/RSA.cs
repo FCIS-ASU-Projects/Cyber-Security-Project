cc9d56c465a1f33d14da441419f318a0b7be7182
Sara Mahmoud <saramahmoud5005@gmail.com>
2023-05-11 00:31:13 +0300

    Update RSA.cs

--- SecurityPackage[Template]/securitylibrary/RSA/RSA.cs
+++ SecurityPackage[Template]/securitylibrary/RSA/RSA.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using SecurityLibrary.AES;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +11,41 @@ namespace SecurityLibrary.RSA
     {
         public int Encrypt(int p, int q, int M, int e)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            int n = p * q;
+            int c = power(M,e,n);
+            return c;
         }
 
         public int Decrypt(int p, int q, int C, int e)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            int n = p * q;
+            int phi = (p - 1) * (q - 1);
+            // To get mod inverse
+            ExtendedEuclid mod = new ExtendedEuclid();
+            int mulInv = mod.GetMultiplicativeInverse(e, phi);
+            int result = power(C, mulInv, n);
+            return result;
+         }
+        
+        public int power(int a, int p,int mod)
+        {
+            if (p == 0) return 1;
+            else if (p == 1) return (a%mod);
+
+            int ret = power(a, p / 2,mod);
+            if (p % 2 == 0)
+            {
+                long res= ((long)ret * (long)ret) % mod;
+                return (int)(res);
+            }
+            else
+            {
+                long res = ((long)ret * (long)ret * (long)a) % mod;
+                return (int)(res);
+            }
         }
     }
 }
+
